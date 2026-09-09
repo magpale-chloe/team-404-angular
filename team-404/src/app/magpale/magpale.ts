833b4770fecafd2008bb2bb6ec0a5cb6fd78a6b5
chloe <preciouschloemagpale@gmail.com>
2026-09-09 12:04:30 +0800

    Add Magpale's Step 1

---
+++ team-404/src/app/magpale/magpale.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-magpale',
+  styleUrl: './magpale.scss',
+  templateUrl: './magpale.html',
+})
+export class Magpale {
+}
