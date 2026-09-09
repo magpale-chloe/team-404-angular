dc4c2c118cf587ff8f2ad4463579681aef29d323
staana-matthew <chonkyseal.work@gmail.com>
2026-09-09 12:55:50 +0800

    feat: call fullname from template file
    Integrate material dependency

--- team-404/src/app/staana/staana.ts
+++ team-404/src/app/staana/staana.ts
@@ -1,8 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-staana',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatDividerModule, MatIconModule],
   templateUrl: './staana.html',
   styleUrl: './staana.scss',
 })
