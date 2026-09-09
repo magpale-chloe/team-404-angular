798f65103c1bf6dc116f689fc4dc690405d8e150
Rullan-Harvey-Laurence <harveylaurencerullan555@gmail.com>
2026-09-09 12:48:38 +0800

    Add Rullan Step 1

---
+++ team-404/src/app/rullan/rullan.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-rullan',
+  styleUrl: './rullan.scss',
+  templateUrl: './rullan.html',
+})
+export class Rullan {}
