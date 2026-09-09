7b7dfa2226b5f4bc856b043574e8672f085b9b75
Rullan-Harvey-Laurence <harveylaurencerullan555@gmail.com>
2026-09-09 10:56:47 +0800

    Add group name to app component and Raquin's step 1

---
+++ team-404/src/app/raquin/raquin.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Raquin } from './raquin';
+
+describe('Raquin', () => {
+  let component: Raquin;
+  let fixture: ComponentFixture<Raquin>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Raquin],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Raquin);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ team-404/src/app/raquin/raquin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-raquin',
+  styleUrl: './raquin.scss',
+  templateUrl: './raquin.html',
+})
+export class Raquin {}
