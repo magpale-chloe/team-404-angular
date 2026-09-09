051e0c3842b856e707aa92fcf20ce7beaec2cde6
Rullan-Harvey-Laurence <harveylaurencerullan555@gmail.com>
2026-09-09 11:57:54 +0800

    Add Raquin's step 5 (MatIconModule in button component)

--- team-404/src/app/raquin/raquin.ts
+++ team-404/src/app/raquin/raquin.ts
@@ -2,13 +2,15 @@ import { Component, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
 	selector: 'app-raquin',
 	imports: [
 		MatButtonModule,
 		MatCardModule,
-		MatDividerModule
+		MatDividerModule,
+		MatIconModule
 	],
 	templateUrl: './raquin.html',
 	styleUrl: './raquin.scss'
