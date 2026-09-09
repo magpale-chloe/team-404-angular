158573b56e27913250ae381da8499f11dca1ea40
Rullan-Harvey-Laurence <harveylaurencerullan555@gmail.com>
2026-09-09 11:22:41 +0800

    Add Raquin's step 2

--- team-404/src/app/raquin/raquin.spec.ts
+++ team-404/src/app/raquin/raquin.spec.ts
@@ -2,20 +2,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Raquin } from './raquin';
 
 describe('Raquin', () => {
-  let component: Raquin;
-  let fixture: ComponentFixture<Raquin>;
+	let component: Raquin;
+	let fixture: ComponentFixture<Raquin>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Raquin],
-    }).compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [Raquin]
+		})
+			.compileComponents();
 
-    fixture = TestBed.createComponent(Raquin);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
-  });
+		fixture = TestBed.createComponent(Raquin);
+		component = fixture.componentInstance;
+		await fixture.whenStable();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- team-404/src/app/raquin/raquin.ts
+++ team-404/src/app/raquin/raquin.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
-  imports: [],
-  selector: 'app-raquin',
-  styleUrl: './raquin.scss',
-  templateUrl: './raquin.html',
+	selector: 'app-raquin',
+	imports: [
+		MatButtonModule,
+		MatCardModule,
+		MatDividerModule,
+	],
+	templateUrl: './raquin.html',
+	styleUrl: './raquin.scss'
 })
-export class Raquin {}
+export class Raquin {
+	fullName = signal('Renz Mathieu M. Raquin');
+	imagePath = signal('assets/images/Raquin-Photo.JPG');
+	role = signal('BSIT Student & Developer');
+	description = signal(
+		'I am Renz Mathieu M. Raquin, a 4th year IT student with a passion for web development and software engineering. I aspire to create systems that can make a positive impact to society.'
+	);
+	isDescriptionVisible = signal(false);
+
+	toggleDescription(): void {
+		this.isDescriptionVisible.update((isVisible) => !isVisible);
+	}
+}
