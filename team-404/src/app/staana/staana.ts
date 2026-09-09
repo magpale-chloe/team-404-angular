4b977f41521e06026361f1d7b5594f586aabfdfa
staana-matthew <chonkyseal.work@gmail.com>
2026-09-09 12:49:20 +0800

    feat: signal staana full name class

--- team-404/src/app/staana/staana.ts
+++ team-404/src/app/staana/staana.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
-  imports: [],
   selector: 'app-staana',
-  styleUrl: './staana.scss',
+  imports: [],
   templateUrl: './staana.html',
+  styleUrl: './staana.scss',
 })
-export class Staana {}
+export class Staana {
+  fullName = signal('Sta. Ana, Matthew E.');
+}
