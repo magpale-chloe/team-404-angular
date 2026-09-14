3c0f89850e33d2b12e292dfcc7d5c88a125b08d7
Rullan-Harvey-Laurence <harveylaurencerullan555@gmail.com>
2026-09-14 09:52:14 +0800

    add member-preview folder

---
+++ team-404/src/app/shared/member-preview/member-preview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-member-preview',
+  styleUrl: './member-preview.scss',
+  templateUrl: './member-preview.html',
+})
+export class MemberPreview {}
